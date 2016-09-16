db7a7b3eeeb7cf239adfb24a6e7ce4f30bd47d8c
Ian Smith <ian_smith_12a@mymts.net>
2016-09-16 18:34:25 -0500

    Started working on a Universal Affine cipher application

---
+++ Universal Affine/Universal Affine/Affine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal_Affine
+{
+    class Affine
+    {
+        public int Shift { get; set; }
+        public int Multiplier { get; set; }
+        public string Content { get; set; }
+        public string Encode()
+        {
+            return "";
+        }
+
+        public string Decode()
+        {
+            return "";
+        }
+    }
+}
---
+++ Universal Affine/Universal Affine/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Universal_Affine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Encode.Click += encode;
+            Decode.Click += decode;
+        }
+
+        public void encode(Object o, RoutedEventArgs e)
+        {
+            try
+            {
+                int shift = int.Parse(Shift.Text);
+                try
+                {
+                    int multiplier = int.Parse(Multiplier.Text);
+                    string text = Content.Text;
+                    Affine cipher = new Affine { Shift = shift, Multiplier = multiplier, Content = text };
+                    Content.Text = cipher.Encode();
+                }
+                catch (FormatException exc)
+                {
+                    Multiplier.Text = exc.Message;
+                }
+            }
+            catch (FormatException exc)
+            {
+                Shift.Text = exc.Message;
+            }
+        }
+
+        public void decode(Object o, RoutedEventArgs e)
+        {
+            try
+            {
+                int shift = int.Parse(Shift.Text);
+                try
+                {
+                    int multiplier = int.Parse(Multiplier.Text);
+                    string text = Content.Text;
+                    Affine cipher = new Affine { Shift = shift, Multiplier = multiplier, Content = text };
+                    Content.Text = cipher.Decode();
+                }
+                catch (FormatException exc)
+                {
+                    Multiplier.Text = exc.Message;
+                }
+            }
+            catch (FormatException exc)
+            {
+                Shift.Text = exc.Message;
+            }
+        }
+    }
+}
